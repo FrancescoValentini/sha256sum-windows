fd0ff5eafed05460b9a34ebfbd377b27d0c229b2
Francesco Valentini <francesco.valentini930@gmail.com>
2025-12-23 14:56:06 +0100

    Added arguments

--- sha256sum-windows/sha256sum-windows.cpp
+++ sha256sum-windows/sha256sum-windows.cpp
@@ -1,12 +1,41 @@
 #include <iostream>
 #include <argparse/argparse.hpp>
+#include "sha256sum-windows.h"
+
+static void setupArguments(argparse::ArgumentParser& program) {
+    // FILE (positional)
+    program.add_argument("files")
+        .help("Files to process, or '-' for standard input")
+        .default_value(std::vector<std::string>{})
+        .nargs(argparse::nargs_pattern::any);
+
+    // Checksum verification mode
+    program.add_argument("-c", "--check")
+        .help("read checksums from the FILEs and check them")
+        .default_value(false)
+        .implicit_value(true);
+
+    // Checksum verification options
+    program.add_argument("--ignore-missing")
+        .help("don't fail or report status for missing files")
+        .default_value(false)
+        .implicit_value(true);
+
+    program.add_argument("--quiet")
+        .help("don't print OK for each successfully verified file")
+        .default_value(false)
+        .implicit_value(true);
+
+    program.add_argument("--status")
+        .help("don't output anything, status code shows success")
+        .default_value(false)
+        .implicit_value(true);
+}
 
 int main(int argc, char* argv[]) {
-    argparse::ArgumentParser program("program_name");
+    argparse::ArgumentParser program(PGM_NAME);
 
-    program.add_argument("square")
-        .help("display the square of a given integer")
-        .scan<'i', int>();
+    setupArguments(program);
 
     try {
         program.parse_args(argc, argv);
@@ -17,8 +46,14 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
-    auto input = program.get<int>("square");
-    std::cout << (input * input) << std::endl;
+    bool check = program.get<bool>("--check");
+    std::vector<std::string> files = program.get<std::vector<std::string>>("files");
 
-    return 0;
+    std::cout << "check: " << check << "\n";
+    std::cout << "files:\n";
+    for (const auto& f : files) {
+        std::cout << "- " << f << "\n";
+    }
+
+    return EXIT_OK;
 }
---
+++ sha256sum-windows/sha256sum-windows.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#define PGM_NAME "sha256sum"
+#define PGM_VERS "1.0.0"
+
+// Exit codes
+#define EXIT_OK        0  // All checksums OK / operation successful
+#define EXIT_MISMATCH  1  // At least one checksum does NOT match
+#define EXIT_ERROR     2  // Error (missing file, invalid format, IO, etc.)
+
+#define BUFFER_SIZE    16384 // bytes
